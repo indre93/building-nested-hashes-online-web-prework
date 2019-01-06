0531c7e057880328b44adf03f5e7765a11343faf
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-06 01:55:42 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,10 +1,16 @@
 def first_challenge
 epic_tragedy = {
   :montague => {
-    :patriarch => {}
-    :matriarch => {}
-    :hero => {}
+    :patriarch => {},
+    :matriarch => {},
+    :hero => {},
+    :hero_friends => []
   },
-  :capulet => {}
+  :capulet => {
+    :patriarch => {},
+    :matriarch => {},
+    :heroine => {},
+    :hero_friends => []
+  }
 }
 end
